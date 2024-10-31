b6032fd9b727837a6649b28501b73b9a69403b4c
Mário Carvalho <mariodearaujocarvalho@gmail.com>
2024-10-31 09:33:50 -0400

    Refactor test_folder_utils.py to improve file counting accuracy and efficiency

--- tools/test_aux/test_folder_utils.py
+++ tools/test_aux/test_folder_utils.py
@@ -3,9 +3,13 @@ import os
 import sys
 
 def count_files(dir_path):
+    count_files_all_folder = 0
     for root, dirs, files in os.walk(dir_path):
         for dir in dirs:
-            print(f'{dir}: {len(os.listdir(os.path.join(root, dir)))}')
-
+            count_files_folder = len(os.listdir(os.path.join(root, dir)))
+            print(f'{dir}: {count_files_folder}')
+            count_files_all_folder += count_files_folder
+    print(f'Total: {count_files_all_folder}')
+    print(f'Average: {count_files_all_folder / len(os.listdir(dir_path))}')
 if __name__ == '__main__':
     count_files(sys.argv[1])
