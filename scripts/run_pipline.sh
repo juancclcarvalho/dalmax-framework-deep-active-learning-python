12a4128c095c8e579fcc6bc5955d2c555c76e9de
Mário Carvalho <mariodearaujocarvalho@gmail.com>
2024-11-29 17:37:47 -0400

    Remove commented-out demo script commands for clarity in run pipeline script

--- scripts/run_pipline.sh
+++ scripts/run_pipline.sh
@@ -22,9 +22,6 @@ N_ROUND=10
 for strategy in "${strategies[@]}"; do
     echo ""
     echo "Running $strategy"
-    #CUDA_VISIBLE_DEVICES=$1 python demo.py --dataset_name=DANINHAS --strategy_name $strategy --n_query 10 --seed $2 && notify-send "Process finished" $strategy
-    #CUDA_VISIBLE_DEVICES=$1 python demo.py --dataset_name=DANINHAS --strategy_name $strategy --n_query 50 --seed $2 && notify-send "Process finished" $strategy
-    #CUDA_VISIBLE_DEVICES=$1 python demo.py --dataset_name=DANINHAS --strategy_name $strategy --n_query 100 --seed $2 && notify-send "Process finished" $strategy
     n_queries=(10 50 100)
     for n_query in "${n_queries[@]}"; do
         echo "Running $strategy with $n_query queries"
