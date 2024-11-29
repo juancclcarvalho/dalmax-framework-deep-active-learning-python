742cbadc8493f85cf00962c286200572f4521ac1
Mário Carvalho <mariodearaujocarvalho@gmail.com>
2024-11-29 17:48:47 -0400

    Enable all sampling strategies in the run pipeline script and update parameters to use params_dnb.json

--- scripts/run_pipline.sh
+++ scripts/run_pipline.sh
@@ -2,11 +2,11 @@
 # Example: bash scripts/run_pipline.sh 0 1
 # Lista de estratégias
 strategies=(
-    #RandomSampling
-    #LeastConfidence
-    #MarginSampling
-    #EntropySampling
-    # LeastConfidenceDropout
+    RandomSampling
+    LeastConfidence
+    MarginSampling
+    EntropySampling
+    LeastConfidenceDropout
     MarginSamplingDropout
     EntropySamplingDropout
     KMeansSampling
@@ -25,6 +25,6 @@ for strategy in "${strategies[@]}"; do
     n_queries=(10 50 100)
     for n_query in "${n_queries[@]}"; do
         echo "Running $strategy with $n_query queries"
-        CUDA_VISIBLE_DEVICES=$1 python demo.py --params_json params_df.json --dataset_name=DANINHAS --strategy_name $strategy --n_query $n_query --seed $2 --n_round $N_ROUND && notify-send "Process finished" $strategy
+        CUDA_VISIBLE_DEVICES=$1 python demo.py --params_json params_dnb.json --dataset_name=DANINHAS --strategy_name $strategy --n_query $n_query --seed $2 --n_round $N_ROUND && notify-send "Process finished" $strategy
     done
 done
