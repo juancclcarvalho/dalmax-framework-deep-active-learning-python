fbb940488903945bdf9b0d26ab4dd1ed663b3c21
Mário Carvalho <mariodearaujocarvalho@gmail.com>
2024-12-03 16:00:35 -0400

    Add new parameter configuration file and update run pipeline script to use it

---
+++ scripts/fix_dnf_seed_1.sh
@@ -0,0 +1,33 @@
+CUDA_VISIBLE_DEVICES=0 python demo.py --params_json params_dnf.json --dataset_name=DANINHAS --strategy_name LeastConfidence --n_query 10 --seed 1 --n_round 10
+CUDA_VISIBLE_DEVICES=0 python demo.py --params_json params_dnf.json --dataset_name=DANINHAS --strategy_name MarginSampling --n_query 10 --seed 1 --n_round 10
+CUDA_VISIBLE_DEVICES=0 python demo.py --params_json params_dnf.json --dataset_name=DANINHAS --strategy_name EntropySampling --n_query 10 --seed 1 --n_round 10
+CUDA_VISIBLE_DEVICES=0 python demo.py --params_json params_dnf.json --dataset_name=DANINHAS --strategy_name LeastConfidenceDropout --n_query 10 --seed 1 --n_round 10
+CUDA_VISIBLE_DEVICES=0 python demo.py --params_json params_dnf.json --dataset_name=DANINHAS --strategy_name MarginSamplingDropout --n_query 10 --seed 1 --n_round 10
+CUDA_VISIBLE_DEVICES=0 python demo.py --params_json params_dnf.json --dataset_name=DANINHAS --strategy_name EntropySamplingDropout --n_query 10 --seed 1 --n_round 10
+CUDA_VISIBLE_DEVICES=0 python demo.py --params_json params_dnf.json --dataset_name=DANINHAS --strategy_name KMeansSampling --n_query 10 --seed 1 --n_round 10
+CUDA_VISIBLE_DEVICES=0 python demo.py --params_json params_dnf.json --dataset_name=DANINHAS --strategy_name KCenterGreedy --n_query 10 --seed 1 --n_round 10
+CUDA_VISIBLE_DEVICES=0 python demo.py --params_json params_dnf.json --dataset_name=DANINHAS --strategy_name BALDDropout --n_query 10 --seed 1 --n_round 10
+
+
+CUDA_VISIBLE_DEVICES=0 python demo.py --params_json params_dnf.json --dataset_name=DANINHAS --strategy_name RandomSampling --n_query 50 --seed 1 --n_round 10
+CUDA_VISIBLE_DEVICES=0 python demo.py --params_json params_dnf.json --dataset_name=DANINHAS --strategy_name LeastConfidence --n_query 50 --seed 1 --n_round 10
+CUDA_VISIBLE_DEVICES=0 python demo.py --params_json params_dnf.json --dataset_name=DANINHAS --strategy_name MarginSampling --n_query 50 --seed 1 --n_round 10
+CUDA_VISIBLE_DEVICES=0 python demo.py --params_json params_dnf.json --dataset_name=DANINHAS --strategy_name EntropySampling --n_query 50 --seed 1 --n_round 10
+CUDA_VISIBLE_DEVICES=0 python demo.py --params_json params_dnf.json --dataset_name=DANINHAS --strategy_name LeastConfidenceDropout --n_query 50 --seed 1 --n_round 10
+CUDA_VISIBLE_DEVICES=0 python demo.py --params_json params_dnf.json --dataset_name=DANINHAS --strategy_name MarginSamplingDropout --n_query 50 --seed 1 --n_round 10
+CUDA_VISIBLE_DEVICES=0 python demo.py --params_json params_dnf.json --dataset_name=DANINHAS --strategy_name EntropySamplingDropout --n_query 50 --seed 1 --n_round 10
+CUDA_VISIBLE_DEVICES=0 python demo.py --params_json params_dnf.json --dataset_name=DANINHAS --strategy_name KMeansSampling --n_query 50 --seed 1 --n_round 10
+CUDA_VISIBLE_DEVICES=0 python demo.py --params_json params_dnf.json --dataset_name=DANINHAS --strategy_name KCenterGreedy --n_query 50 --seed 1 --n_round 10
+CUDA_VISIBLE_DEVICES=0 python demo.py --params_json params_dnf.json --dataset_name=DANINHAS --strategy_name BALDDropout --n_query 50 --seed 1 --n_round 10
+
+
+CUDA_VISIBLE_DEVICES=0 python demo.py --params_json params_dnf.json --dataset_name=DANINHAS --strategy_name RandomSampling --n_query 100 --seed 1 --n_round 10
+CUDA_VISIBLE_DEVICES=0 python demo.py --params_json params_dnf.json --dataset_name=DANINHAS --strategy_name LeastConfidence --n_query 100 --seed 1 --n_round 10
+CUDA_VISIBLE_DEVICES=0 python demo.py --params_json params_dnf.json --dataset_name=DANINHAS --strategy_name MarginSampling --n_query 100 --seed 1 --n_round 10
+CUDA_VISIBLE_DEVICES=0 python demo.py --params_json params_dnf.json --dataset_name=DANINHAS --strategy_name EntropySampling --n_query 100 --seed 1 --n_round 10
+CUDA_VISIBLE_DEVICES=0 python demo.py --params_json params_dnf.json --dataset_name=DANINHAS --strategy_name LeastConfidenceDropout --n_query 100 --seed 1 --n_round 10
+CUDA_VISIBLE_DEVICES=0 python demo.py --params_json params_dnf.json --dataset_name=DANINHAS --strategy_name MarginSamplingDropout --n_query 100 --seed 1 --n_round 10
+CUDA_VISIBLE_DEVICES=0 python demo.py --params_json params_dnf.json --dataset_name=DANINHAS --strategy_name EntropySamplingDropout --n_query 100 --seed 1 --n_round 10
+CUDA_VISIBLE_DEVICES=0 python demo.py --params_json params_dnf.json --dataset_name=DANINHAS --strategy_name KMeansSampling --n_query 100 --seed 1 --n_round 10
+CUDA_VISIBLE_DEVICES=0 python demo.py --params_json params_dnf.json --dataset_name=DANINHAS --strategy_name KCenterGreedy --n_query 100 --seed 1 --n_round 10
+CUDA_VISIBLE_DEVICES=0 python demo.py --params_json params_dnf.json --dataset_name=DANINHAS --strategy_name BALDDropout --n_query 100 --seed 1 --n_round 10
--- scripts/run_pipline.sh
+++ scripts/run_pipline.sh
@@ -18,13 +18,44 @@ strategies=(
 
 N_ROUND=10
 
+echo ""
 # Loop pelas estratégias
 for strategy in "${strategies[@]}"; do
-    echo ""
-    echo "Running $strategy"
-    n_queries=(10 50 100)
+    
+    # echo "Running $strategy"
+    n_queries=(10)
     for n_query in "${n_queries[@]}"; do
-        echo "Running $strategy with $n_query queries"
-        CUDA_VISIBLE_DEVICES=$1 python demo.py --params_json params_dnb.json --dataset_name=DANINHAS --strategy_name $strategy --n_query $n_query --seed $2 --n_round $N_ROUND && notify-send "Process finished" $strategy
+        # echo "Running $strategy with $n_query queries"
+        echo CUDA_VISIBLE_DEVICES=$1 python demo.py --params_json params_dnf.json --dataset_name=DANINHAS --strategy_name $strategy --n_query $n_query --seed $2 --n_round $N_ROUND
     done
 done
+
+echo ""
+echo ""
+
+# Loop pelas estratégias
+for strategy in "${strategies[@]}"; do
+    # echo "Running $strategy"
+    n_queries=(50)
+    for n_query in "${n_queries[@]}"; do
+        # echo "Running $strategy with $n_query queries"
+        echo CUDA_VISIBLE_DEVICES=$1 python demo.py --params_json params_dnf.json --dataset_name=DANINHAS --strategy_name $strategy --n_query $n_query --seed $2 --n_round $N_ROUND
+    done
+done
+
+echo ""
+echo ""
+
+# Loop pelas estratégias
+for strategy in "${strategies[@]}"; do
+    # echo "Running $strategy"
+    n_queries=(100)
+    for n_query in "${n_queries[@]}"; do
+        # echo "Running $strategy with $n_query queries"
+        echo CUDA_VISIBLE_DEVICES=$1 python demo.py --params_json params_dnf.json --dataset_name=DANINHAS --strategy_name $strategy --n_query $n_query --seed $2 --n_round $N_ROUND
+    done
+done
+
+# Example: CUDA_VISIBLE_DEVICES=1 python demo.py --params_json params_dnf.json --dataset_name=DANINHAS --strategy_name LeastConfidenceDropout --n_query 100 --seed 2 --n_round 10
+echo ""
+echo ""
